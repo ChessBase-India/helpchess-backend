824bcdb36457ee76d361bbd5bacd940249b4b2e6
Adarsh Bhadauria <theadxy@gmail.com>
2026-08-17 22:33:33 +0530

    refactor: upgrade logger to support variadic arguments and improved object formatting

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,77 +1,88 @@
+const util = require('util');
 const VError = require('verror');
 
 const winstonLogger = require('utils/winstonLogger');
 
-// eslint-disable-next-line no-underscore-dangle
-const _log = ({ logObject, logLevel = 'info' }) => {
-  if (!logObject) {
+// compact + breakLength keep one log call on one line; without compact,
+// util.inspect still wraps deeply nested objects.
+const formatArg = (arg) => {
+  if (typeof arg === 'string') {
+    return arg;
+  }
+  if (arg instanceof Error) {
+    return VError.fullStack(arg);
+  }
+  return util.inspect(arg, { depth: 4, breakLength: Infinity, compact: true });
+};
+
+// Levels below warning are promoted so an Error can't hide in a DEBUG line,
+// which the file transport drops.
+const severityFor = ({ args, logLevel }) => {
+  if (!args.some((arg) => arg instanceof Error)) {
+    return logLevel;
+  }
+  return winstonLogger.levels[logLevel] <= winstonLogger.levels.warning ? logLevel : 'error';
+};
+
+const writeLog = ({ args, logLevel = 'info' }) => {
+  if (!args.length || args.every((arg) => arg === null || arg === undefined)) {
     return;
   }
 
-  if (typeof logObject !== 'object') {
-    winstonLogger[logLevel](logObject);
-    return;
-  }
+  const [logObject] = args;
 
-  // If errorObject is not an instance of Error, log the object at given log level.
-  if (!(logObject instanceof Error)) {
-    winstonLogger[logLevel](logObject);
-    return;
-  }
-
-  const { vErrorLogLevel } = VError.info(logObject);
-  if (vErrorLogLevel && Object.prototype.hasOwnProperty.call(winstonLogger.levels, logLevel)) {
-    if (winstonLogger.levels[vErrorLogLevel] <= winstonLogger.levels.warning) {
-      winstonLogger[vErrorLogLevel](VError.fullStack(logObject));
-    } else {
-      winstonLogger[vErrorLogLevel](logObject.message);
+  // vErrorLogLevel overrides the requested severity, but only for a lone Error:
+  // with context args there is no single error to take the override from.
+  if (args.length === 1 && logObject instanceof Error) {
+    const { vErrorLogLevel } = VError.info(logObject);
+    if (Object.prototype.hasOwnProperty.call(winstonLogger.levels, vErrorLogLevel)) {
+      if (winstonLogger.levels[vErrorLogLevel] <= winstonLogger.levels.warning) {
+        winstonLogger[vErrorLogLevel](VError.fullStack(logObject));
+      } else {
+        winstonLogger[vErrorLogLevel](logObject.message);
+      }
+      return;
     }
-    return;
   }
 
-  // errorObject is an instance of Error but has no vErrorLogLevel specified.
-  if (winstonLogger.levels[logLevel] <= winstonLogger.levels.warning) {
-    winstonLogger[logLevel](VError.fullStack(logObject));
-    return;
-  }
-  winstonLogger.error(VError.fullStack(logObject));
+  winstonLogger[severityFor({ args, logLevel })](args.map(formatArg).join(' '));
 };
 
 const logger = {
-  log: (logObject) => {
-    _log({ logObject });
+  log: (...args) => {
+    writeLog({ args });
   },
 
-  debug: (logObject) => {
-    _log({ logObject, logLevel: 'debug' });
+  debug: (...args) => {
+    writeLog({ args, logLevel: 'debug' });
   },
 
-  info: (logObject) => {
-    _log({ logObject, logLevel: 'info' });
+  info: (...args) => {
+    writeLog({ args, logLevel: 'info' });
   },
 
-  notice: (logObject) => {
-    _log({ logObject, logLevel: 'notice' });
+  notice: (...args) => {
+    writeLog({ args, logLevel: 'notice' });
   },
 
-  warning: (logObject) => {
-    _log({ logObject, logLevel: 'warning' });
+  warning: (...args) => {
+    writeLog({ args, logLevel: 'warning' });
   },
 
-  error: (logObject) => {
-    _log({ logObject, logLevel: 'error' });
+  error: (...args) => {
+    writeLog({ args, logLevel: 'error' });
   },
 
-  crit: (logObject) => {
-    _log({ logObject, logLevel: 'crit' });
+  crit: (...args) => {
+    writeLog({ args, logLevel: 'crit' });
   },
 
-  alert: (logObject) => {
-    _log({ logObject, logLevel: 'alert' });
+  alert: (...args) => {
+    writeLog({ args, logLevel: 'alert' });
   },
 
-  emerg: (logObject) => {
-    _log({ logObject, logLevel: 'emerg' });
+  emerg: (...args) => {
+    writeLog({ args, logLevel: 'emerg' });
   }
 };
 
